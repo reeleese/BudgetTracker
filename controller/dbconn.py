2d873888e1f2e6abcc534671215814f64ae553a6
reeleese <leereese3@gmail.com>
2022-02-21 15:18:18 -0600

    created dbconn module

--- controller/dbconn.py
+++ controller/dbconn.py
@@ -1,5 +1,9 @@
 import mysql.connector
 
+def get_password():
+    with open('db_password.txt', 'r') as f:
+        return f.read()
+
 def get_user_db(user):
-    mydb = mysql.connector.connect(host='35.222.151.28', user='root', password='budget-tracker', database=user)
+    mydb = mysql.connector.connect(host='35.222.151.28', user='root', password=get_password(), database=user)
     return mydb
